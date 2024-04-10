1c822df846efa69f7e68feea064a6f8f988b7454
Alex Shevlyakov <pxl.alexjedi@gmail.com>
2024-04-10 17:23:46 +0100

    Add style injections

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
-import dts from "vite-plugin-dts";
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig, UserConfig } from "vite";
+import dts from 'vite-plugin-dts'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig, UserConfig } from 'vite'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 export default defineConfig({
-  base: "./",
-  plugins: [dts({ rollupTypes: true }), react()],
+  base: './',
+  plugins: [dts({ rollupTypes: true }), react(), libInjectCss()],
   build: {
     sourcemap: true,
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "mylib",
-      formats: ["es", "cjs", "umd", "iife"],
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'mylib',
+      formats: ['es', 'cjs', 'umd', 'iife'],
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
   },
-} satisfies UserConfig);
+} satisfies UserConfig)
